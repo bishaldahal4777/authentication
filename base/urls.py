7cd33ba3555b9c66629c5a06b57d4157ee916068
Bishal Dahal <bishaldahal4777@gmail.com>
2025-10-31 16:44:22 +0545

    signuo views

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-
+from . import views
 urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
+    path('signup/', views.auth_view, name="auth_view")
 
 
 ]
--- base/views.py
+++ base/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+
+
+def auth_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST or None)
+        if form.is_valid():
+            form.save()
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/signup.html',{'form':form})

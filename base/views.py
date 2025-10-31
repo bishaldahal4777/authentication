ee6df695142faada4d91eed55cded5663ce3b46c
Bishal Dahal <bishaldahal4777@gmail.com>
2025-10-31 17:22:56 +0545

    views final

--- base/views.py
+++ base/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.contrib.auth.models import User
 
 def login_user(request):
     if request.method == 'POST':
@@ -23,7 +24,20 @@ def logout_user(request):
     return render('login')
 
 def register_user(request):
-    pass
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
 
+        if User.objects.filter(username=username).exists():
+            messages.error(request, "Username Already taken")
+            return redirect('register')
+        user = User.objects.create_user(username=username,email=email,password=password)
+        user.save()
+        messages.success(request, 'Account created successfully! Login now.')
+        return redirect('login')
+    return render(request, 'base/register.html')
+
+@login_required
 def dashboard(request):
-    pass
+    return render(request, 'base/dashboard.html')

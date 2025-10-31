9a2020d11066b6b5a57644a5e502caf99959ec6c
Bishal Dahal <bishaldahal4777@gmail.com>
2025-10-31 17:17:18 +0545

    html template

--- base/urls.py
+++ base/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('login/', views.login_user, name='login'),
-    # path('logout/', views.logout_user, name='logout'),
-    # path('register/', views.register_user, name='register'),
-    # path('dashboard/', views.dashboard, name='dashboard'),
+    path('logout/', views.logout_user, name='logout'),
+    path('register/', views.register_user, name='register'),
+    path('dashboard/', views.dashboard, name='dashboard'),
 
 
 ]

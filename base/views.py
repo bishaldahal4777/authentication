3e815bea296fa284fcdce760ee646a0d9b078aea
Bishal Dahal <bishaldahal4777@gmail.com>
2025-10-31 17:08:52 +0545

    views and html

--- base/views.py
+++ base/views.py
@@ -1,4 +1,19 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('dashboard')
+        else:
+            messages.error(request,'Invalid Username or password')
+            return redirect('login')
+    return render(request, 'base/login.html')

1ad7b61f3eaa150995fe2464d05acb46937f29f2
Bishal Dahal <bishaldahal4777@gmail.com>
2025-10-31 17:32:40 +0545

    views final

--- base/views.py
+++ base/views.py
@@ -21,13 +21,18 @@ def login_user(request):
 
 def logout_user(request):
     logout(request)
-    return render('login')
+    return redirect('login')
 
 def register_user(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         email = request.POST.get('email')
         password = request.POST.get('password')
+        confirm_password = request.POST.get('confirm_password')
+
+        if password != confirm_password:
+            messages.error(request, 'Passwords do not matched ')
+            return redirect('register')
 
         if User.objects.filter(username=username).exists():
             messages.error(request, "Username Already taken")

e909ee2e2b82607cdcbbf9fa5de2cbcdf1f9af88
Bishal Dahal <bishaldahal4777@gmail.com>
2025-11-01 08:41:44 +0545

    read dara html

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,12 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.any_view, name = "any"),
-    path('create', views.create_view, name="create"),
-    path('read', views.read_view, name="read"),
-    path('update/<int:id>', views.update_view, name="update"),
-    path('delete/<int:id>', views.delete_view, name="delete"),
-
-
-
+    path('', views.any_view, name='any'),
+    path('create/', views.create_view, name='create'),
+    path('read/', views.read_view, name='read'),
+    path('update/<int:id>/', views.update_view, name='update'),
+    path('delete/<int:id>/', views.delete_view, name='delete'),
 ]

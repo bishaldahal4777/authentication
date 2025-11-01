4377e50a4cc2dd172c708ceaf3bba2a1f31ce051
Bishal Dahal <bishaldahal4777@gmail.com>
2025-11-01 08:35:04 +0545

    update url

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.any_view, name = "any"),
     path('create', views.create_view, name="create"),
     path('read', views.read_view, name="read"),
+    path('update/<int:id>', views.update_view, name="update"),
+
 
 ]

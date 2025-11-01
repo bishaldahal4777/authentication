5a409674bfde3126c8d64f7dd4de986bc638ed73
Bishal Dahal <bishaldahal4777@gmail.com>
2025-11-01 08:36:06 +0545

    delete  url fix

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('create', views.create_view, name="create"),
     path('read', views.read_view, name="read"),
     path('update/<int:id>', views.update_view, name="update"),
-    path('delete/<int:id>', views.delete, name="delete"),
+    path('delete/<int:id>', views.delete_view, name="delete"),
 
 
 
